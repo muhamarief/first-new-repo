5215152d6606aa04eadaec01f8474453cd5301d7
ariefrahman <arief.rahman279@yahoo.com>
2017-03-17 15:13:18 +0800

    add form create

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,14 +1,9 @@
 class Url < ApplicationRecord
 
   validates :long_url, presence: true
-  before_create :shorten, :default_count
 
   def shorten
    self.short_url = SecureRandom.hex(3)
-  end
-
-  def default_count
-    self.click_count = 0
   end
 
 end
